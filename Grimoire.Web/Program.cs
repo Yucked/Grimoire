4dd50753bad94048d7be40b5b4e397593cfb1568
Yucked <yucked@pm.me>
2023-06-11 22:40:34 -0400

    fixed refs

--- Grimoire.Web/Program.cs
+++ Grimoire.Web/Program.cs
@@ -4,13 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
+    .AddGrimoireProviders()
     .AddHttpClient()
     .AddLogging(x => {
         x.ClearProviders();
         x.AddConsole();
-    })
-    .AddSingleton<Manhwa18Provider>()
-    .AddSingleton<TCBScansProvider>();
+    });
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment()) {
