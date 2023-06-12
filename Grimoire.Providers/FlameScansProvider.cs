e2b43b3f9b585a406b2687a24b8ff75f3d4da460
Yucked <mail@dvlp.nill.sh>
2023-06-12 18:05:37 -0400

    Flame Scans completed

---
+++ Grimoire.Providers/FlameScansProvider.cs
@@ -0,0 +1,95 @@
+﻿using AngleSharp.Html.Dom;
+using Grimoire.Providers.Interfaces;
+using Grimoire.Providers.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Grimoire.Providers;
+
+public class FlameScansProvider : IGrimoireProvider {
+    public string Name
+        => "Flame Scans";
+
+    public string BaseUrl
+        => "https://flamescans.org";
+
+    public string Icon
+        => $"{BaseUrl}/favicon.ico";
+
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<FlameScansProvider> _logger;
+
+    public FlameScansProvider(HttpClient httpClient, ILogger<FlameScansProvider> logger) {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<IReadOnlyList<Manga>> FetchMangasAsync() {
+        using var document = await _httpClient.ParseAsync($"{BaseUrl}/series/list-mode/");
+        var results = document.GetElementsByClassName("blix")
+            .SelectMany(x => x.Children[1].Children)
+            .Select(async x => {
+                var manga = new Manga {
+                    Name = x.FirstChild.TextContent,
+                    Url = (x.FirstChild as IHtmlAnchorElement).Href,
+                    Provider = GetType().Name[..^8],
+                    LastFetch = DateTimeOffset.Now
+                };
+
+                using var doc = await _httpClient.ParseAsync(manga.Url);
+                manga.Author = doc.QuerySelectorAll("div.imptdt")
+                    .FirstOrDefault(c => c.TextContent.Contains("Author"))
+                    .TextContent
+                    .Replace("Author", string.Empty)
+                    .Trim().Clean();
+                manga.Cover = (doc.QuerySelector("div.thumb > img") as IHtmlImageElement).Source;
+                manga.Summary = doc.QuerySelector("div.wd-full > div.entry-content > p").TextContent.Clean();
+                manga.Genre = doc.QuerySelector("div.wd-full > span.mgen").TextContent.Split(' ');
+                manga.Metonyms = doc.QuerySelector("div.desktop-titles").TextContent.Split(',');
+                manga.Chapters = doc.GetElementById("chapterlist")
+                    .FirstChild
+                    .ChildNodes
+                    .Where(c => c is IHtmlListItemElement)
+                    .Select(c => {
+                        var anchor = (c as IHtmlElement).Children[0] as IHtmlAnchorElement;
+                        return new MangaChapter {
+                            Name = anchor.GetElementsByClassName("chapternum").FirstOrDefault().TextContent.Clean(),
+                            Url = anchor.Href,
+                            ReleasedOn = DateOnly.Parse(
+                                anchor.GetElementsByClassName("chapterdate").FirstOrDefault().TextContent)
+                        };
+                    })
+                    .ToArray();
+                return manga;
+            });
+
+        return await Task.WhenAll(results);
+    }
+
+    public Task<IReadOnlyList<Manga>> PaginateAsync(int page) {
+        throw new NotSupportedException("Data is fetched via list mode.");
+    }
+
+    public async Task<IReadOnlyList<MangaChapter>> FetchChaptersAsync(Manga manga) {
+        try {
+            using var document = await _httpClient.ParseAsync(manga.Url);
+            return document.GetElementById("chapterlist")
+                .FirstChild
+                .ChildNodes
+                .Where(x => x is IHtmlListItemElement)
+                .Select(x => {
+                    var anchor = (x as IHtmlElement).Children[0] as IHtmlAnchorElement;
+                    return new MangaChapter {
+                        Name = anchor.GetElementsByClassName("chapternum").FirstOrDefault().TextContent.Clean(),
+                        Url = anchor.Href,
+                        ReleasedOn = DateOnly.Parse(
+                            anchor.GetElementsByClassName("chapterdate").FirstOrDefault().TextContent)
+                    };
+                })
+                .ToArray();
+        }
+        catch (Exception exception) {
+            _logger.LogError("{exception}\n{message}", exception, exception.Message);
+            throw;
+        }
+    }
+}
