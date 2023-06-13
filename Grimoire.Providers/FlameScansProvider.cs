60ad114982fd3d8f9904a3a450d5b06ff826e41b
Yucked <mail@dvlp.nill.sh>
2023-06-12 20:16:13 -0400

    try catch for FlameScansProvider.cs

--- Grimoire.Providers/FlameScansProvider.cs
+++ Grimoire.Providers/FlameScansProvider.cs
@@ -35,30 +35,36 @@ public class FlameScansProvider : IGrimoireProvider {
                     LastFetch = DateTimeOffset.Now
                 };
 
-                using var doc = await _httpClient.ParseAsync(manga.Url);
-                manga.Author = doc.QuerySelectorAll("div.imptdt")
-                    .FirstOrDefault(c => c.TextContent.Contains("Author"))
-                    .TextContent
-                    .Replace("Author", string.Empty)
-                    .Trim().Clean();
-                manga.Cover = (doc.QuerySelector("div.thumb > img") as IHtmlImageElement).Source;
-                manga.Summary = doc.QuerySelector("div.wd-full > div.entry-content > p").TextContent.Clean();
-                manga.Genre = doc.QuerySelector("div.wd-full > span.mgen").TextContent.Split(' ');
-                manga.Metonyms = doc.QuerySelector("div.desktop-titles").TextContent.Split(',');
-                manga.Chapters = doc.GetElementById("chapterlist")
-                    .FirstChild
-                    .ChildNodes
-                    .Where(c => c is IHtmlListItemElement)
-                    .Select(c => {
-                        var anchor = (c as IHtmlElement).Children[0] as IHtmlAnchorElement;
-                        return new MangaChapter {
-                            Name = anchor.GetElementsByClassName("chapternum").FirstOrDefault().TextContent.Clean(),
-                            Url = anchor.Href,
-                            ReleasedOn = DateOnly.Parse(
-                                anchor.GetElementsByClassName("chapterdate").FirstOrDefault().TextContent)
-                        };
-                    })
-                    .ToArray();
+                try {
+                    using var doc = await _httpClient.ParseAsync(manga.Url);
+                    manga.Author = doc.QuerySelectorAll("div.imptdt")
+                        .FirstOrDefault(c => c.TextContent.Contains("Author"))
+                        .TextContent
+                        .Replace("Author", string.Empty)
+                        .Trim().Clean();
+                    manga.Cover = (doc.QuerySelector("div.thumb > img") as IHtmlImageElement).Source;
+                    manga.Summary = doc.QuerySelector("div.wd-full > div.entry-content").TextContent.Clean();
+                    manga.Genre = doc.QuerySelector("div.wd-full > span.mgen").TextContent.Split(' ');
+                    manga.Metonyms = doc.QuerySelector("div.desktop-titles")?.TextContent.Split(',');
+                    manga.Chapters = doc.GetElementById("chapterlist")
+                        .FirstChild
+                        .ChildNodes
+                        .Where(c => c is IHtmlListItemElement)
+                        .Select(c => {
+                            var anchor = (c as IHtmlElement).Children[0] as IHtmlAnchorElement;
+                            return new MangaChapter {
+                                Name = anchor.GetElementsByClassName("chapternum").FirstOrDefault().TextContent.Clean(),
+                                Url = anchor.Href,
+                                ReleasedOn = DateOnly.Parse(
+                                    anchor.GetElementsByClassName("chapterdate").FirstOrDefault().TextContent)
+                            };
+                        })
+                        .ToArray();
+                }
+                catch (Exception exception) {
+                    _logger.LogError("Failed to parse html for {url}\n{exception}", manga.Url, exception);
+                }
+
                 return manga;
             });
 
