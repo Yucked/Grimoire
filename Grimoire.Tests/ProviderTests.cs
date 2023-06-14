d1faa4163336f7c7ddf092cd8b6b7608a2d1d693
Yucked <yucked@pm.me>
2023-06-13 22:58:35 -0400

    chapter tests

--- Grimoire.Tests/ProviderTests.cs
+++ Grimoire.Tests/ProviderTests.cs
@@ -9,7 +9,15 @@ namespace Grimoire.Tests;
 public sealed class ProviderTests {
     private readonly Type _provider
         = typeof(FlameScansProvider);
-    
+
+    private static Manga Manga => new() {
+        Url = "https://flamescans.org/series/1686564121-forty-eight-hours-a-day/"
+    };
+
+    private static MangaChapter Chapter = new() {
+        Url = "https://tcbscans.com/chapters/43/bleach-chapter-686.5"
+    };
+
     [TestMethod]
     public async Task FetchMangasAsync() {
         var provider = Globals.Services.GetRequiredService(_provider) as IGrimoireProvider;
@@ -25,12 +33,18 @@ public sealed class ProviderTests {
         var provider = Globals.Services.GetRequiredService(_provider) as IGrimoireProvider;
         Assert.IsNotNull(provider);
 
-        var manga = new Manga {
-            Url = "https://flamescans.org/series/1686564121-forty-eight-hours-a-day/"
-        };
-        
-        var chapters = await provider.FetchChaptersAsync(manga);
+        var chapters = await provider.FetchChaptersAsync(Manga);
         Assert.IsNotNull(chapters);
         Assert.IsTrue(chapters.Count > 0);
+    }
+
+    [TestMethod]
+    public async Task GetChapterAsync() {
+        var provider = Globals.Services.GetRequiredService<TCBScansProvider>();
+        var chapter = await provider.GetChapterAsync(Chapter);
+
+        Assert.IsNotNull(chapter);
+        Assert.IsNotNull(chapter.Pages);
+        Assert.IsTrue(chapter.Pages.Count > 0);
     }
 }
