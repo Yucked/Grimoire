9566fdb657f422ec4dafe9b3818ab4215c8e6dd0
Yucked <yucked@pm.me>
2023-06-14 23:24:08 -0400

    idk what the hell was the last commit

---
+++ Grimoire.Db/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using Grimoire.Providers.Interfaces;
+using StackExchange.Redis;
+
+namespace Grimoire.Db;
+
+public static class Helpers {
+    public static string GetId(this IGrimoireProvider provider) {
+        return provider.Name.Replace(' ', '_');
+    }
+
+    internal static byte[] Encode<T>(this T value) {
+        return JsonSerializer.SerializeToUtf8Bytes(value);
+    }
+
+    internal static T Decode<T>(this RedisValue value) {
+        return JsonSerializer.Deserialize<T>(value);
+    }
+}
