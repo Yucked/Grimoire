0a096ec4718c31ba3d24e25c6c3435ee9e43aae1
Yucked <yucked@pm.me>
2023-06-14 22:51:54 -0400

    CacheOptions.cs

---
+++ Grimoire.Db/CacheOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Grimoire.Db; 
+
+public struct CacheOptions {
+    public string RedisHost { get; init; }
+    public string RedisPort { get; init; }
+    public string SaveTo { get; init; }
+}
