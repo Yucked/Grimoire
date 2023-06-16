e9d528923c839891f4da40419db1ff22f1d8c5ea
Yucked <mail@dvlp.nill.sh>
2023-06-16 17:36:29 -0400

    pkgs update

--- Grimoire.Db/CacheOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Grimoire.Db; 
-
-public record CacheOptions {
-    public string RedisHost { get; init; }
-    public string RedisPort { get; init; }
-    public string SaveTo { get; init; }
-}
