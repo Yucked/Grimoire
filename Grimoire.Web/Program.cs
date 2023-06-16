a9959b71484823463e06de9c115ac5b12d1ec003
Yucked <mail@dvlp.nill.sh>
2023-06-16 17:32:40 -0400

    moved waiting component to another file

--- Grimoire.Web/Program.cs
+++ Grimoire.Web/Program.cs
@@ -1,10 +1,24 @@
 using Grimoire.Providers;
+using Grimoire.Web;
+using Grimoire.Web.Cache;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.Sources.Clear();
+builder.Configuration
+    .AddJsonFile("settings.json");
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
+    .AddSingleton<CacheHandler>()
+    .AddSingleton(new CacheOptions {
+        SaveTo = Path.Combine(builder.Environment.ContentRootPath, "static")
+    })
+    .AddSingleton<AppState>()
     .AddGrimoireProviders()
+    .AddResponseCaching()
     .AddHttpClient()
     .AddLogging(x => {
         x.ClearProviders();
@@ -17,9 +31,20 @@ if (!app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+var provider = new PhysicalFileProvider(app.Services.GetRequiredService<CacheOptions>().SaveTo);
+app.Environment.WebRootFileProvider = new CompositeFileProvider(
+    new PhysicalFileProvider(builder.Environment.WebRootPath),
+    provider
+);
+
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions {
+    FileProvider = provider,
+    RequestPath = "/static"
+});
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-app.Run();
+await app.RunAsync();
