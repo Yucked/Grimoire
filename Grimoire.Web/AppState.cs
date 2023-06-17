c861bd8bc2a8be0c9658bacedc172bfa9713d902
Yucked <mail@dvlp.nill.sh>
2023-06-16 20:27:02 -0400

    removed AppState.cs and settings.json, renamed prov -> source, MainLayout.razor - > Base.razor

--- Grimoire.Web/AppState.cs
+++
@@ -1,9 +0,0 @@
-﻿using Grimoire.Providers.Models;
-
-namespace Grimoire.Web;
-
-public class AppState {
-    public IReadOnlyList<Manga> Mangas { get; set; }
-    public Manga Manga { get; set; }
-    public MangaChapter Chapter { get; set; }
-}
