691c435345b3bae222559a96b38fcf922058d630
Yucked <yucked@pm.me>
2023-06-16 23:51:17 -0400

    Added PathMaker class for building paths

---
+++ Grimoire.Web/PathMaker.cs
@@ -0,0 +1,38 @@
+﻿namespace Grimoire.Web;
+
+public record struct PathMaker {
+    public static PathMaker New(string basePath) => new(basePath);
+
+    public string Ave { get; private set; }
+
+    private PathMaker(string basePath) {
+        Ave = basePath;
+    }
+
+    public PathMaker WithSource(string sourceId) {
+        Ave = Path.Combine(Ave, sourceId);
+        return this;
+    }
+
+    public PathMaker WithManga(string mangaId) {
+        Ave = Path.Combine(Ave, mangaId);
+        return this;
+    }
+
+    public string WithIcon(string sourceIcon) {
+        return Path.Combine(Ave, sourceIcon.Split('/')[^1]);
+    }
+
+    public string WithCover(string sourceIcon) {
+        return Path.Combine(Ave, sourceIcon.Split('/')[^1]);
+    }
+
+    public PathMaker Verify() {
+        if (Directory.Exists(Ave)) {
+            return this;
+        }
+
+        Directory.CreateDirectory(Ave!);
+        return this;
+    }
+}
