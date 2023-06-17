d9f92d470b48533f6525cba926eed09dd451632c
Yucked <yucked@pm.me>
2023-06-16 20:28:51 -0400

    remove CacheOptions & Helper, use IConfiguration to get data from config

--- Grimoire.Web/Cache/CacheHandler.cs
+++ Grimoire.Web/Cache/CacheHandler.cs
@@ -1,11 +1,9 @@
-﻿using Grimoire.Providers;
-using Grimoire.Providers.Interfaces;
-using Grimoire.Providers.Models;
+﻿using Grimoire.Sources;
+using Grimoire.Sources.Interfaces;
+using Grimoire.Sources.Models;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Grimoire.Web.Cache;
-
-public readonly record struct CachedProvider(string Id, string Name, string IconPath, IReadOnlyList<Manga> Mangas);
 
 public record CachedManga(string Id, string CoverPath) : Manga;
 
@@ -13,76 +11,53 @@ public sealed class CacheHandler {
     private readonly IMemoryCache _memoryCache;
     private readonly IServiceProvider _serviceProvider;
     private readonly HttpClient _httpClient;
-    private readonly CacheOptions _options;
+    private readonly IConfiguration _config;
 
     public CacheHandler(IMemoryCache memoryCache,
                         IServiceProvider serviceProvider,
                         HttpClient httpClient,
-                        CacheOptions options) {
+                        IConfiguration config) {
         _memoryCache = memoryCache;
         _serviceProvider = serviceProvider;
         _httpClient = httpClient;
-        _options = options;
+        _config = config;
     }
 
-    public async Task<IReadOnlyList<CachedProvider>> GetProvidersAsync() {
-        var providers = _serviceProvider.GetGrimoireProviders();
-        var tasks = providers.Select(GetProviderAsync);
+    public async Task<IReadOnlyList<IGrimoireSource>> GetSourcesAsync() {
+        var providers = _serviceProvider.GetGrimoireSources();
+        var tasks = providers.Select(GetSourceAsync);
         return await Task.WhenAll(tasks);
     }
 
-    public async Task<CachedProvider> GetProviderAsync(IGrimoireProvider prov) {
-        if (_memoryCache.TryGetValue(prov.Name.GetId(), out CachedProvider cachedProvider)) {
-            return cachedProvider;
+    public async Task<IGrimoireSource> GetSourceAsync(IGrimoireSource source) {
+        if (_memoryCache.TryGetValue(source.Id, out IGrimoireSource sourceCache)) {
+            return sourceCache;
         }
 
-        var basePath = _options.SaveTo.GetBasePath(prov.Name.GetId());
-        var iconPath = basePath.GetIconPath(prov.Icon);
+        var basePath = _config["SaveTo"].GetBasePath(source.Id);
+        var iconPath = basePath.GetIconPath(source.Icon);
         if (!Directory.Exists(basePath)) {
             Directory.CreateDirectory(basePath!);
         }
 
         if (!File.Exists(iconPath)) {
-            await _httpClient.DownloadAsync(prov.Icon, basePath);
+            await _httpClient.DownloadAsync(source.Icon, basePath);
         }
 
-        var mangas = await prov.FetchMangasAsync();
-        var tasks = mangas.Select(async x => {
-            var mPath = Path.Combine(basePath, x.Name.GetId());
-            var coverPath = mPath.GetIconPath(x.Cover);
+        _memoryCache.Set(source.Id, sourceCache);
 
-            if (!Directory.Exists(mPath)) {
-                Directory.CreateDirectory(mPath!);
-            }
-
-            if (!File.Exists(coverPath)) {
-                await _httpClient.DownloadAsync(x.Cover, $"{prov.Name.GetId()}@{x.Name.GetId()}");
-            }
-
-            return new CachedManga($"{prov.Name.GetId()}@{x.Name.GetId()}", coverPath);
-        });
-
-        cachedProvider = new CachedProvider {
-            Id = prov.Name.GetId(),
-            Name = prov.Name,
-            IconPath = iconPath,
-            Mangas = await Task.WhenAll(tasks)
-        };
-
-        _memoryCache.Set(prov.Name.GetId(), cachedProvider);
-
-        return cachedProvider;
+        return sourceCache;
     }
 
-    public async Task<CachedManga> GetMangaAsync(CachedProvider prov, string name) {
-        if (_memoryCache.TryGetValue($"{prov.Name.GetId()}@{name.GetId()}", out byte[] data)) {
-            return data.Decode<CachedManga>();
+    public async Task<CachedManga> GetMangaAsync(IGrimoireSource source, string mangaId) {
+        if (_memoryCache.TryGetValue($"{source.Id}_{mangaId}", out byte[] data)) {
+            return default;
         }
 
         return default;
     }
 
     public string GetStaticPath(string str) {
-        return str.Replace(_options.SaveTo, "static");
+        return str.Replace(_config["SaveTo"]!, "static");
     }
 }
