456ab74febcb37fbc837ee94eed1b4de8034086e
Yucked <mail@dvlp.nill.sh>
2023-06-16 23:51:51 -0400

    misc methods and updates

---
+++ Grimoire.Web/Misc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Grimoire.Web;
+
+public static class Misc {
+    public static string GetCover(this IConfiguration configuration, string localPath, string url) {
+        return configuration.GetValue<bool>("Save:MangaCover")
+            ? localPath
+            : url;
+    }
+
+    public static bool IsNullOrEmpty(this IList list) {
+        return list == null || list.Count == 0;
+    }
+
+    public static bool IsNullOrEmpty<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary) {
+        return dictionary == null || dictionary.Count == 0;
+    }
+}
