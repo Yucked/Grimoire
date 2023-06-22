6bdcbe3b223bb429271586c054d2f6725d29a6cc
Yucked <mail@dvlp.nill.sh>
2023-06-21 22:57:29 -0400

    separated extension to misc files and added fetchchapterasync exten method
    Signed-off-by: Yucked <yucked@pm.me>

---
+++ Grimoire.Sources/Misc.DI.cs
@@ -0,0 +1,32 @@
+﻿using Grimoire.Sources.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grimoire.Sources;
+
+public static partial class Misc {
+    public static IServiceCollection AddGrimoireSources(this IServiceCollection collection) {
+        var sources = typeof(IGrimoireSource).Assembly
+            .GetTypes()
+            .Where(x => typeof(IGrimoireSource).IsAssignableFrom(x)
+                        && !x.IsInterface
+                        && !x.IsAbstract);
+
+        foreach (var source in sources) {
+            collection.AddSingleton(source);
+        }
+
+        return collection;
+    }
+
+    public static IEnumerable<IGrimoireSource> GetGrimoireSources(this IServiceProvider provider) {
+        var sources = typeof(IGrimoireSource).Assembly
+            .GetTypes()
+            .Where(x => typeof(IGrimoireSource).IsAssignableFrom(x)
+                        && !x.IsInterface
+                        && !x.IsAbstract);
+
+        return sources
+            .Select(provider.GetRequiredService)
+            .OfType<IGrimoireSource>();
+    }
+}
