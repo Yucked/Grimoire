39a4d5dc1aeb57709815a9482ffa2a80f88485b5
Yucked <mail@dvlp.nill.sh>
2023-07-03 21:05:06 -0400

    user library model and handler
    Signed-off-by: Yucked <yucked@pm.me>

---
+++ Grimoire.Web/Handlers/Models/UserLibrary.cs
@@ -0,0 +1,10 @@
+﻿namespace Grimoire.Web.Handlers.Models;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed record UserLibrary {
+    // ReSharper disable once MemberCanBeMadeStatic.Global
+    public string Id
+        => nameof(Grimoire);
+
+    public List<string> Favourites { get; set; }
+}
