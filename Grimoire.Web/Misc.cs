8d05c6ce200c25ec2021fda46c951652a60fa726
Yucked <mail@dvlp.nill.sh>
2023-07-08 20:21:22 -0400

    load thumbnails properly
    Signed-off-by: Yucked <yucked@pm.me>

--- Grimoire.Web/Misc.cs
+++ Grimoire.Web/Misc.cs
@@ -1,26 +1,17 @@
-﻿using System.Collections;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Grimoire.Web;
 
 public static class Misc {
     public static string GetCover(this IConfiguration configuration, string localPath, string url) {
-        return configuration.GetValue<bool>("Save:MangaCover")
+        return configuration.GetValue<bool>("Save:MangaCover") && !string.IsNullOrWhiteSpace(localPath)
             ? localPath
             : url;
     }
 
-    public static bool IsNullOrEmpty(this IList list) {
-        return list == null || list.Count == 0;
-    }
-
-    public static bool IsNullOrEmpty<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary) {
-        return dictionary == null || dictionary.Count == 0;
-    }
-
     public static async Task<bool> DoesCollectionExistAsync(this IMongoDatabase database,
-                                                     string collectionName) {
+                                                            string collectionName) {
         var filter = new BsonDocument("name", collectionName);
         var collections = await database.ListCollectionsAsync(
             new ListCollectionsOptions {
