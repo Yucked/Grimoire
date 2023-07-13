1d7dcc12cc93e6dfaaabbfb6f3a35d2e9f6e62e3
Yucked <yucked@pm.me>
2023-07-13 00:28:36 -0400

    html parser with rotating proxies, random user-agents
    Signed-off-by: Yucked <yucked@pm.me>

---
+++ Grimoire.Commons/Parsing/ParserOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Grimoire.Commons.Parsing;
+
+public readonly record struct ParserOptions(int MaxDelay,
+                                            int MaxRetries,
+                                            string[] Proxies,
+                                            string[] UserAgents);
