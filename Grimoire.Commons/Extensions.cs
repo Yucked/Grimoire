870ba8601836dd660a173d37302e708767153a0a
Yucked <mail@dvlp.nill.sh>
2023-07-13 00:28:43 -0400

    ya some extensions
    Signed-off-by: Yucked <yucked@pm.me>

---
+++ Grimoire.Commons/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Grimoire.Commons.Parsing;
+using Grimoire.Commons.Proxy;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Grimoire.Commons;
+
+public static class Extensions {
+    public static string GetIdFromName(this string name) {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(name));
+    }
+
+    public static string GetNameFromId(this string id) {
+        return Encoding.UTF8.GetString(Convert.FromBase64String(id));
+    }
+
+    public static T RandomItem<T>(this T[] items) {
+        return items[Random.Shared.Next(items.Length - 1)];
+    }
+
+    public static T RandomItem<T>(this IReadOnlyList<T> items) {
+        return items[Random.Shared.Next(items.Count - 1)];
+    }
+
+    public static IServiceCollection AddHtmlParser(this IServiceCollection collection, ParserOptions options) {
+        return collection
+            .AddSingleton<ProxiesHandler>()
+            .AddSingleton(x => new HtmlParser(
+                x.GetRequiredService<ILogger<HtmlParser>>(),
+                x.GetRequiredService<ProxiesHandler>(),
+                options
+            ));
+    }
+}
