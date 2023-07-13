ac2b51d3014bafc2570655fcc9ce0f4741569190
Yucked <mail@dvlp.nill.sh>
2023-07-13 19:05:47 -0400

    trying out htmlparser
    Signed-off-by: Yucked <yucked@pm.me>

--- Grimoire.Sources/Sources/TCBScansSource.cs
+++ Grimoire.Sources/Sources/TCBScansSource.cs
@@ -1,7 +1,9 @@
 using AngleSharp.Html.Dom;
-using Grimoire.Sources.Interfaces;
+using Grimoire.Commons;
+using Grimoire.Commons.Interfaces;
+using Grimoire.Commons.Models;
+using Grimoire.Commons.Parsing;
 using Grimoire.Sources.Miscellaneous;
-using Grimoire.Sources.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Grimoire.Sources.Sources;
@@ -17,13 +19,15 @@ public sealed class TCBScansSource : IGrimoireSource {
         => $"{BaseUrl}/files/apple-touch-icon.png";
 
     private readonly ILogger<TCBScansSource> _logger;
+    private readonly HtmlParser _htmlParser;
 
-    public TCBScansSource(ILogger<TCBScansSource> logger) {
+    public TCBScansSource(ILogger<TCBScansSource> logger, HtmlParser htmlParser) {
         _logger = logger;
+        _htmlParser = htmlParser;
     }
 
     public async Task<IReadOnlyList<Manga>> FetchMangasAsync() {
-        using var document = await Misc.ParseAsync($"{BaseUrl}/projects");
+        using var document = await _htmlParser.ParseAsync($"{BaseUrl}/projects");
         var tasks = document
             .QuerySelectorAll("a.mb-3.text-white")
             .AsParallel()
@@ -32,7 +36,7 @@ public sealed class TCBScansSource : IGrimoireSource {
     }
 
     public async Task<Manga> GetMangaAsync(string url) {
-        using var document = await Misc.ParseAsync(url);
+        using var document = await _htmlParser.ParseAsync(url);
         return new Manga {
             Name = document.QuerySelector("div.px-4 > h1").TextContent.Clean(),
             Url = url,
@@ -56,7 +60,7 @@ public sealed class TCBScansSource : IGrimoireSource {
     }
 
     public async Task<IReadOnlyList<Chapter>> FetchChaptersAsync(Manga manga) {
-        using var document = await Misc.ParseAsync(manga.Url);
+        using var document = await _htmlParser.ParseAsync(manga.Url);
         _logger.LogDebug("Fetching chapters for {name}", manga.Name);
         return document.GetElementsByClassName("block border border-border bg-card mb-3 p-3 rounded")
             .Select(x => new Chapter {
@@ -67,7 +71,7 @@ public sealed class TCBScansSource : IGrimoireSource {
     }
 
     public async Task<Chapter> FetchChapterAsync(Chapter chapter) {
-        using var document = await Misc.ParseAsync(chapter.Url);
+        using var document = await _htmlParser.ParseAsync(chapter.Url);
         chapter.Pages = document
             .QuerySelectorAll("img.fixed-ratio-content")
             .Select(x => (x as IHtmlImageElement).Source)
