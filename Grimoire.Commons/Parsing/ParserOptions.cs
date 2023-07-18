404b7383483c5220bdecb84b51a57dacd387fd2d
Yucked <yucked@pm.me>
2023-07-18 12:25:56 -0400

    removed parser options

--- Grimoire.Commons/Parsing/ParserOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Grimoire.Commons.Parsing;
-
-public sealed record ParserOptions(int MaxDelay,
-                                  int MaxRetries,
-                                  string[] Proxies,
-                                  string[] UserAgents);
