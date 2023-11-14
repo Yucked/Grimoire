0755c7e7673edaff35d7fda8dc786be6dd03bba9
Yucked <mail@dvlp.nill.sh>
2023-11-14 00:42:14 -0500

    nullables wtf

--- src/Sources/OmegaScansSource.cs
+++ src/Sources/OmegaScansSource.cs
@@ -1,10 +1,13 @@
+using System.Text.Json;
 using Grimoire.Handlers;
 using Grimoire.Models;
 using Grimoire.Sources.Interfaces;
 
 namespace Grimoire.Sources;
 
-public class OmegaScansSource : IGrimoireSource {
+public class OmegaScansSource(
+    HttpHandler httpHandler,
+    ILogger<OmegaScansSource> logger) : IGrimoireSource {
     public string Name
         => "Omega Scans";
 
@@ -14,14 +17,44 @@ public class OmegaScansSource : IGrimoireSource {
     public string Icon
         => "https://omegascans.org/icon.png";
 
-    private readonly HttpHandler _httpHandler;
+    private readonly Dictionary<string, (string Name, string Cover, string Genre)> _apiCache = new();
 
     public async Task<IReadOnlyList<Manga>> GetMangasAsync() {
-        throw new NotImplementedException();
+        var stream =
+            await httpHandler.GetStreamAsync($"{Url}/query?visibility=Public&series_type=All&perPage=100");
+        using var document = await JsonDocument.ParseAsync(stream!);
+        if (!document.RootElement.TryGetProperty("data", out var dataElement)) {
+            logger.LogError("Unable to fetch JSON data from source!");
+            return null;
+        }
+
+        var tasks = dataElement
+            .EnumerateArray()
+            .AsParallel()
+            .Select(x => {
+                var slug = x.GetProperty("series_slug").GetString()!;
+                _apiCache.Add(slug,
+                    (x.GetProperty("title").GetString()!,
+                     x.GetProperty("thumbnail").GetString()!,
+                     x.GetProperty("series_type").GetString()!));
+
+                return GetMangaAsync($"https://omegascans.org/series/{slug}");
+            });
+
+        var mangas = await Task.WhenAll(tasks);
+        _apiCache.Clear();
+        return mangas;
     }
 
     public async Task<Manga> GetMangaAsync(string url) {
-        throw new NotImplementedException();
+        var document = await httpHandler.ParseAsync(url);
+        var manga = new Manga {
+            Summary = document
+                .QuerySelector("div.bg-gray-800 > p")
+                .TextContent
+        };
+
+        return manga;
     }
 
     public async Task<Chapter> FetchChapterAsync(Chapter chapter) {
