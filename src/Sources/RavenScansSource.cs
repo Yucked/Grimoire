e0c7124149f227d25e003af5852b46d51897d715
Yucked <yucked@pm.me>
2023-11-19 15:05:02 -0500

    sources cleanup

--- src/Sources/RavenScansSource.cs
+++ src/Sources/RavenScansSource.cs
@@ -7,7 +7,7 @@ namespace Grimoire.Sources;
 
 // "img.ts-main-image"
 public class RavenScansSource(
-    ILogger<ArenaScansSource> logger,
+    ILogger<RavenScansSource> logger,
     HttpHandler httpHandler) : IGrimoireSource {
     public string Name
         => "Raven Scans";
@@ -18,13 +18,13 @@ public class RavenScansSource(
     public string Icon
         => "https://i0.wp.com/ravenscans.com/wp-content/uploads/2022/12/cropped-33.png";
 
-    public Task<IReadOnlyList<Manga>?> GetMangasAsync() {
+    public Task<IReadOnlyList<Manga>> GetMangasAsync() {
         return WordPressAbstraction
             .Helper(logger, httpHandler, Name, Url)
             .GetMangasAsync();
     }
 
-    public Task<Manga?> GetMangaAsync(string url) {
+    public Task<Manga> GetMangaAsync(string url) {
         return WordPressAbstraction
             .Helper(logger, httpHandler, Name, Url)
             .GetMangaAsync(url);
