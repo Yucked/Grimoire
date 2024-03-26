664f40a68ac5466786e62b4e2a8f5887457797cc
Yucked <mail@dvlp.nill.sh>
2024-03-26 00:12:47 -0400

    Indexes

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,5 @@
+using Grimoire.Handlers;
+using Grimoire.Objects;
 using LiteDB;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -5,13 +7,25 @@ builder.Configuration.Sources.Clear();
 builder.Configuration.AddJsonFile("config.json", false, true);
 
 builder.Services.AddControllers();
-builder.Services.AddOutputCache();
-builder.Services.AddMemoryCache(x =>
-{
-    x.CompactionPercentage = 80;
-    x.ExpirationScanFrequency = TimeSpan.FromMinutes(30);
-});
-builder.Services.AddSingleton<ILiteDatabase>(new LiteDatabase("grimoire.db"));
+builder.Services
+    .AddOutputCache()
+    .AddMemoryCache(x => {
+        x.CompactionPercentage = 0.80f;
+        x.ExpirationScanFrequency = TimeSpan.FromMinutes(30);
+    })
+    .AddSingleton<DatabaseHandler>()
+    .AddSingleton<ILiteDatabase>(x => {
+        var database = new LiteDatabase("grimoire.db");
+        var collection = database.GetCollection<MangaObject>();
+        collection.EnsureIndex(e => e.Aliases);
+        collection.EnsureIndex(e => e.Artists);
+        collection.EnsureIndex(e => e.Authors);
+        collection.EnsureIndex(e => e.Title);
+        collection.EnsureIndex(e => e.Summary);
+        collection.EnsureIndex(e => e.Genres);
+        return database;
+    })
+    .AddHostedService<BackgroundHandler>();
 
 var app = builder.Build();
 app.MapControllers();
