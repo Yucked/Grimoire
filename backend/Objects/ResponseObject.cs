f5ca2059f3534352b2c925f0c16e985ec7682561
Yucked <mail@dvlp.nill.sh>
2024-03-26 00:12:29 -0400

    Moved ResponseObject.cs to Objects and renamed

---
+++ backend/Objects/ResponseObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Grimoire.Objects;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="StatusCode"></param>
+/// <param name="Data"></param>
+public record ResponseObject(int StatusCode, object Data = null!) {
+    public static ResponseObject New(int statusCode, object data = null!)
+        => new(statusCode, data);
+}
