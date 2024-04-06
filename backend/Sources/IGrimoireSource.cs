80618d48b16d67938daa2726f7dad5fbfe5c4eca
Yucked <mail@dvlp.nill.sh>
2024-04-06 14:07:57 -0400

    initial test for playwright with tcbscan

---
+++ backend/Sources/IGrimoireSource.cs
@@ -0,0 +1,35 @@
+﻿using Grimoire.Objects;
+
+namespace Grimoire.Sources;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IGrimoireSource {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Task<IReadOnlyList<MangaObject>> GetMangasAsync();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Task<MangaObject> GetMangaAsync(string url);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="manga"></param>
+    /// <returns></returns>
+    Task<MangaObject> GetMangaAsync(MangaObject manga)
+        => GetMangaAsync(manga.SourceUrl);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="chapter"></param>
+    /// <returns></returns>
+    Task<ChapterObject> FetchChapterAsync(ChapterObject chapter);
+}
---
+++ backend/Sources/TCBScansSource.cs
@@ -0,0 +1,51 @@
+﻿using Grimoire.Handlers;
+using Grimoire.Objects;
+
+namespace Grimoire.Sources;
+
+public sealed class TCBScansSource(ScrapingHandler scrapingHandler) : IGrimoireSource {
+    public async Task<IReadOnlyList<MangaObject>> GetMangasAsync() {
+        var page = await scrapingHandler.RequestPageAsync("https://tcbscans.com/projects");
+        var links = await page.QuerySelectorAllAsync("a.mb-3.text-white");
+
+        var results = links
+            .Select(async element => {
+                var href = await element.GetAttributeAsync("href");
+                return await GetMangaAsync($"https://tcbscans.com{href}");
+            })
+            .AwaitAsync();
+
+        await page.CloseAsync();
+        return await results;
+    }
+
+    public async Task<MangaObject> GetMangaAsync(string url) {
+        var page = await scrapingHandler.RequestPageAsync(url);
+        var name = await page
+            .QuerySelectorAsync("div.px-4 > h1")!
+            .GetTextContentAsync();
+        var cover = await page
+            .QuerySelectorAsync("div.flex > img")!
+            .GetAttributeAsync("src");
+
+        new MangaObject()
+        return default;
+    }
+
+    public async Task<ChapterObject> FetchChapterAsync(ChapterObject chapter) {
+        var page = await scrapingHandler.RequestPageAsync(chapter.SourceUrl);
+        var mangaPages = await page.QuerySelectorAllAsync("img.fixed-ratio-content");
+        var sources = await mangaPages
+            .Select(x => x.GetAttributeAsync("source"))
+            .AwaitAsync();
+
+        foreach (var (k, v) in sources
+                     .Select((x, i) => new { x, i })
+                     .ToDictionary(x => x.i, x => x.x)) {
+            chapter.Pages.Add(k, new PageObject(false, default!, v!));
+        }
+
+        // TODO: Maybe store it in database directly?
+        return chapter;
+    }
+}
