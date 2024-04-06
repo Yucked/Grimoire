0d352045075b339b6f61622121d1dc8d26c55f82
Yucked <mail@dvlp.nill.sh>
2024-04-06 14:08:45 -0400

    need to automate browser download

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,17 @@
 using Grimoire.Handlers;
 using Grimoire.Objects;
+using Grimoire.Sources;
 using LiteDB;
+using Microsoft.Playwright;
+
+var playwright = await Playwright.CreateAsync();
+var browser = await playwright.Chromium
+    .LaunchAsync(new BrowserTypeLaunchOptions {
+        ExecutablePath = Path.Combine(
+            Environment.CurrentDirectory,
+            "ChromeHeadlessShell",
+            "chrome-headless-shell.exe")
+    });
 
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.Configuration.Sources.Clear();
@@ -13,6 +24,7 @@ builder.Services
         x.CompactionPercentage = 0.80f;
         x.ExpirationScanFrequency = TimeSpan.FromMinutes(30);
     })
+    .AddHttpClient()
     .AddSingleton<DatabaseHandler>()
     .AddSingleton<ScrapingHandler>()
     .AddSingleton<ILiteDatabase>(x => {
@@ -26,7 +38,9 @@ builder.Services
         collection.EnsureIndex(e => e.Genres);
         return database;
     })
-    .AddHostedService<BackgroundHandler>();
+    .AddHostedService<BackgroundHandler>()
+    .AddSingleton<TCBScansSource>()
+    .AddSingleton(browser);
 
 var app = builder.Build();
 app.MapControllers();
