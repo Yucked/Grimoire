4b67402990a197600411621d0ab3473396e59439
Yucked <mail@dvlp.nill.sh>
2025-11-02 18:47:05 -0500

    Removed LiteDB, added RavenDB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,50 +1,66 @@
+using FlareSolverrSharp;
+using Grimoire;
 using Grimoire.Handlers;
-using Grimoire.Objects;
+using Grimoire.Services;
 using Grimoire.Sources;
-using LiteDB;
 using Microsoft.Playwright;
+using Raven.Client.Documents;
 
-var exitCode = Microsoft.Playwright.Program.Main(["install"]);
-if (exitCode != 0) {
-    throw new Exception($"Playwright exited with code {exitCode}");
+public sealed class Program {
+    private static async Task Main(string[] args) {
+        var browserPath = Path.Combine(Directory.GetCurrentDirectory(), "playwright");
+        Environment.SetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH", browserPath);
+
+        if (!Directory.Exists(Path.Combine(browserPath, "chromium-*"))) {
+            var exitCode = Microsoft.Playwright.Program.Main(
+                ["install",
+                "--with-deps",
+                "chromium"]);
+
+            if (exitCode != 0) {
+                throw new Exception($"Playwright exited with code {exitCode}");
+            }
+        }
+
+        var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync();
+
+        var builder = WebApplication.CreateSlimBuilder(args);
+        builder.Configuration.Sources.Clear();
+        builder.Configuration.AddJsonFile("config.json", false, true);
+
+        builder.Services.AddControllers();
+        builder.Services
+            .AddOutputCache()
+            .AddHostedService<DatabaseBackgroundService>()
+            .AddHostedService<MangaBackgroundService>()
+            .AddSingleton<ServiceCoodrinator>()
+            .AddSingleton<DatabaseHandler>()
+            .AddSingleton<ScrapingHandler>()
+            .AddSingleton<TCBScansSource>()
+            .AddSingleton(browser)
+            .AddSingleton(x => new DocumentStore {
+                Urls = builder.Configuration.GetSection("RavenNodes").Get<string[]>(),
+                Conventions = {
+            CreateHttpClient = _ => x.GetService<IHttpClientFactory>()!.CreateClient("RavenDB"),
+            UseOptimisticConcurrency = true,
+            MaxNumberOfRequestsPerSession = 30,
+            RequestTimeout = TimeSpan.FromSeconds(15)
+                },
+                Database = nameof(Grimoire)
+            }.Initialize())
+            .AddHttpClient<ScrapingHandler>()
+            .ConfigurePrimaryHttpMessageHandler(() =>
+            new ClearanceHandler(builder.Configuration.GetValue<string>("Http:FlareUrl")) {
+                MaxTimeout = builder.Configuration.GetValue<int>("Http:FlareTimeout")
+            });
+
+        var app = builder.Build();
+        app.MapControllers();
+        app.UseOutputCache(); // what does it do?
+                              // app.UseResponseCaching();
+                              // app.UseResponseCompression();
+
+        await app.RunAsync();
+    }
 }
-
-var playwright = await Playwright.CreateAsync();
-var browser = await playwright.Chromium.LaunchAsync();
-
-var builder = WebApplication.CreateSlimBuilder(args);
-builder.Configuration.Sources.Clear();
-builder.Configuration.AddJsonFile("config.json", false, true);
-
-builder.Services.AddControllers();
-builder.Services
-    .AddOutputCache()
-    .AddMemoryCache(x => {
-        x.CompactionPercentage = 0.80f;
-        x.ExpirationScanFrequency = TimeSpan.FromMinutes(30);
-    })
-    .AddHttpClient()
-    .AddSingleton<DatabaseHandler>()
-    .AddSingleton<ScrapingHandler>()
-    .AddSingleton<ILiteDatabase>(x => {
-        var database = new LiteDatabase("grimoire.db");
-        var collection = database.GetCollection<MangaObject>();
-        collection.EnsureIndex(e => e.Aliases);
-        collection.EnsureIndex(e => e.Artists);
-        collection.EnsureIndex(e => e.Authors);
-        collection.EnsureIndex(e => e.Title);
-        collection.EnsureIndex(e => e.Summary);
-        collection.EnsureIndex(e => e.Genres);
-        return database;
-    })
-    .AddHostedService<BackgroundHandler>()
-    .AddSingleton<TCBScansSource>()
-    .AddSingleton(browser);
-
-var app = builder.Build();
-app.MapControllers();
-app.UseOutputCache(); // what does it do?
-// app.UseResponseCaching();
-// app.UseResponseCompression();
-
-await app.RunAsync();
