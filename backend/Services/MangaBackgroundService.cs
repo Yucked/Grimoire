4c2e3cb19180718da70577419f52996ff8129f0c
Yucked <mail@dvlp.nill.sh>
2025-11-02 18:45:10 -0500

    renamed BackgroundHandler to MangaBackgroundService

--- backend/Services/MangaBackgroundService.cs
+++ backend/Services/MangaBackgroundService.cs
@@ -1,33 +1,34 @@
-﻿using Grimoire.Objects;
+using Grimoire.Handlers;
+using Grimoire.Objects;
 using Grimoire.Sources;
-using LiteDB;
 using Microsoft.Playwright;
 
-namespace Grimoire.Handlers;
+namespace Grimoire.Services;
 
-public sealed class BackgroundHandler(
-    ILiteDatabase database,
+public sealed class MangaBackgroundService(
     IConfiguration configuration,
-    ILogger<BackgroundHandler> logger,
-    ScrapingHandler scrapingHandler,
+    ILogger<MangaBackgroundService> logger,
+    DatabaseHandler databaseHandler,
     IBrowser browser,
+    ServiceCoodrinator serviceCoodrinator,
     TCBScansSource tcbScansSource) : BackgroundService {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        await serviceCoodrinator.WaitForServiceAsync(stoppingToken);
         await UpdateSourcesInDatabaseAsync();
-        
-        var collection = database.GetCollection<SourceObject>("Sources");
+
+        var sources = await databaseHandler.GetSourcesAysnc();
         while (browser.IsConnected && !stoppingToken.IsCancellationRequested) {
-            await Parallel.ForEachAsync(collection.FindAll(), stoppingToken, async (source, token) => {
+            await Parallel.ForEachAsync(sources, stoppingToken, async (source, token) => {
                 if (source.IsDisabled) {
                     logger.LogWarning("Skipping disabled source {}", source.Name);
                     return;
                 }
-                
+
                 if (DateTime.Now.Subtract(source.UpdatedOn) < TimeSpan.FromMinutes(30)) {
                     logger.LogDebug("Skipping {}; updated fetched less than 30 minutes ago.", source.Name);
                     return;
                 }
-                
+
                 logger.LogWarning("{} is outdated. Fetching latest information...", source.Name);
                 if (source.Name.Contains("TCB")) {
                     var mangaObjects = await tcbScansSource.GetMangasAsync();
@@ -35,23 +36,19 @@ public sealed class BackgroundHandler(
                         logger.LogWarning("Failed to fetch mangas from {}", source.Name);
                         return;
                     }
-                    
-                    var mangaSource = database.GetCollection<MangaObject>(source.Id);
-                    mangaSource.InsertBulk(mangaObjects);
+
+                    await databaseHandler.BulkStoreAsync(mangaObjects);
                 }
             });
         }
     }
-    
+
     private Task UpdateSourcesInDatabaseAsync() {
         var sources = configuration
             .GetSection("Sources")
             .Get<IReadOnlyCollection<SourceObject>>()!;
         logger.LogInformation("Current number of sources in config: {}", sources.Count);
-        
-        var collection = database.GetCollection<SourceObject>("Sources");
-        var result = collection.InsertBulk(sources.Where(x => !collection.Exists(y => y.Id == x.Id)));
-        logger.LogInformation("Inserted {} sources in database.", result);
-        return Task.CompletedTask;
+
+        return databaseHandler.BulkStoreAsync(sources);
     }
 }
