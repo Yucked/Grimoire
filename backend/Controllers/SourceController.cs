1b8c27f5c05c00264db2454bd7a1b529e59a9eec
Yucked <mail@dvlp.nill.sh>
2026-02-25 15:33:52 -0500

    file rename

--- backend/Controllers/SourceController.cs
+++ backend/Controllers/SourceController.cs
@@ -7,24 +7,24 @@ namespace Grimoire.Controllers;
 [ApiController,
  Route("api/[controller]"),
  Produces("application/json")]
-public sealed class SourcesController(
+public sealed class SourceController(
     DatabaseHandler databaseHandler) : ControllerBase {
     [HttpGet]
     public async ValueTask<ResponseObject> GetAsync() {
         var sources = await databaseHandler.GetSourcesAysnc();
-        return ResponseObject.New(StatusCodes.Status200OK, sources);
+        return sources.Count == 0
+            ? ResponseObject.New(StatusCodes.Status204NoContent)
+            : ResponseObject.New(StatusCodes.Status200OK, sources);
     }
 
     [HttpGet("{sourceId}")]
     public async ValueTask<ResponseObject> GetAsync(string sourceId) {
-        var source = await databaseHandler.GetMangasAsync(sourceId);
-        return source.Count == 0
-            ? ResponseObject.New(StatusCodes.Status404NotFound)
-            : await source.AsResponseAsync(StatusCodes.Status200OK);
+        var source = await databaseHandler.GetSourceAysnc(sourceId);
+        return ResponseObject.New(StatusCodes.Status200OK, source);
     }
 
-    [HttpPut]
-    public async ValueTask<ResponseObject> PutAsync(SourceObject source) {
+    [HttpPost]
+    public async ValueTask<ResponseObject> AddAsync(SourceObject source) {
         await databaseHandler.StoreAsync(source);
         return ResponseObject.New(StatusCodes.Status200OK);
     }
