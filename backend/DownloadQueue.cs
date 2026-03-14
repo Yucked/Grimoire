5d71239caf944b59de784a8338326fded30922f5
Yucked <mail@dvlp.nill.sh>
2026-03-14 16:00:29 -0400

    DownloadQueue.cs based on Channel

---
+++ backend/DownloadQueue.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace Grimoire;
+
+public record DownloadQueue {
+    private readonly Channel<(string SourceId, string MangaId, string ChapterNumber, string[] ImageUrls)> _channel;
+
+    public ChannelReader<(string SourceId, string MangaId, string ChapterNumber, string[] ImageUrls)>
+        Reader => _channel.Reader;
+
+    public DownloadQueue() {
+        _channel =
+            Channel.CreateUnbounded<(string SourceId, string MangaId, string ChapterNumber, string[] ImageUrls)>();
+    }
+
+    public ValueTask AddAsync(string sourceId, string mangaId, string chapterNumber, string[] imageUrls) {
+        return _channel.Writer.WriteAsync((sourceId, mangaId, chapterNumber, imageUrls));
+    }
+}
